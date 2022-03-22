2e8945a7c4b388abf9fca063aa6cf1d762e56f40
unknown <kinekarpallavi21@gmail.com>
2022-03-22 14:04:46 +0530

    Adding details and display details

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 namespace AddressBookSystem
 {//Uc1
-    class ContactInfo
+    class contactInfo
     {
         public string FirstName;
         public string LastName;
         public string Email;
-        public string PhoneNumber;
+        public int PhoneNumber;
         public string Address;
         public string City;
         public string State;
-        public string ZipCode;
-        public ContactInfo(string FirstName, string LastName, string Email, string PhoneNumber, string Address, string City, string State, string ZipCode)
+        public int ZipCode;
+        public contactInfo(string FirstName, string LastName, string Email, int PhoneNumber, string Address, string City, string State, int ZipCode)
         {
             this.FirstName = FirstName;
             this.LastName = LastName;
@@ -23,13 +25,29 @@ namespace AddressBookSystem
             this.State = State;
             this.ZipCode = ZipCode;
         }
-      
+        //Uc2
+        public string toString()
+        {
+            return " Information Of " + FirstName + " " + LastName + " are: " +"\n"
+                                                                              + "Email: " + Email +"\n"+ "PhoneNumber: " + PhoneNumber +"\n" 
+                                                                                    +"Address: " + Address +"\n"+ "City:" + City+ "\n"+ "State:" + State +"\n" 
+                                                                                    + "ZipCode:" + ZipCode + "\n";
+        }
+
+
     }
+
     class Program
     {
-        public static void Main(string[] args)
+        private ArrayList ContactInfoList;
+        private Dictionary<string, contactInfo> ContactInfoMap;
+        public Program()
         {
-            Console.WriteLine("Welcome to Address Book");
+            ContactInfoList = new ArrayList();
+            ContactInfoMap = new Dictionary<string, contactInfo>();
+        }
+        public void AddInfo()
+        {
             Console.WriteLine("Enter your first name:");
             string FirstName = Console.ReadLine();
             Console.WriteLine("Enter your last name:");
@@ -37,7 +55,7 @@ namespace AddressBookSystem
             Console.WriteLine("Enter your email:");
             string Email = Console.ReadLine();
             Console.WriteLine("Enter your Phone number:");
-            string PhoneNumber = Console.ReadLine();
+            int PhoneNumber =Convert.ToInt32 (Console.ReadLine());
             Console.WriteLine("Enter your Address:");
             string Address = Console.ReadLine();
             Console.WriteLine("Enter your City:");
@@ -45,8 +63,52 @@ namespace AddressBookSystem
             Console.WriteLine("Enter your State:");
             string State = Console.ReadLine();
             Console.WriteLine("Enter your Zipcode:");
-            string ZipCode = Console.ReadLine();
+            int ZipCode =Convert.ToInt32(Console.ReadLine());
+            contactInfo contactInfo = new contactInfo(FirstName, LastName, Email, PhoneNumber, Address, City, State, ZipCode);
+            ContactInfoList.Add(contactInfo);
+            ContactInfoMap.Add(FirstName, contactInfo);
+        }
+        public void DisplayInfo()
+        {
+            foreach (contactInfo contact in ContactInfoList)
+                Console.WriteLine(contact.toString());
+        }
 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            int option = 0;
+            Program Info = new Program();
+            do
+            {
+                Console.WriteLine("1 for Adding Infomarmation");
+                Console.WriteLine("2 for Display Infomarmation");
+                Console.WriteLine("0 for Exit");
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Info.AddInfo();
+                            break;
+                        case 2:
+                            Info.DisplayInfo();
+                            break;
+                        case 0:
+                            Console.WriteLine("Exit");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please Choose option");
+                }
+            }
+            while (option != 0);
+            }
         }
     }
-}
+            
+
+
